1603156427f97e07e5abc85a03fbc1d8f48b7c2f
MartinLadecky <m.ladecky@gmail.com>
2023-05-09 22:14:34 +0200

    milestone 2: implemented verlet step 1 and 2
    no testet yet

--- src/verlet.cpp
+++ src/verlet.cpp
@@ -2,11 +2,24 @@
 
 void verlet_step1(double &x, double &y, double &z, double &vx, double &vy, double &vz,
                   double fx, double fy, double fz, double timestep) {
+
+    vx += 0.5 * fx * timestep; // TODO{Martin} Where is mass?
+    vy += 0.5 * fy * timestep;
+    vz += 0.5 * fz * timestep;
+
+    x += vx * timestep;
+    y += vy * timestep;
+    z += vz * timestep;
+
     // ... implement Verlet step1 here ...
 }
 
 void verlet_step2(double &vx, double &vy, double &vz, double fx, double fy, double fz,
                   double timestep) {
-  //  ... implement Verlet step2 here ...
+
+    vx += 0.5 * fx * timestep; // TODO{Martin} Where is mass?
+    vy += 0.5 * fy * timestep;
+    vz += 0.5 * fz * timestep;
+    //  ... implement Verlet step2 here ...
 }
 
